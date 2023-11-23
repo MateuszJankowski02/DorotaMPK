f0f4f2c5707f534f89590217ea3f49dac0d4bca3
MateuszJankowski02 <twonightdragons@gmail.com>
2023-11-23 17:48:28 +0100

    Added licznikCzynnosciSerwisowych incrementation

--- src/Biletomat/Serwis.java
+++ src/Biletomat/Serwis.java
@@ -9,25 +9,31 @@ public class Serwis {
 
     protected Serwis(Biletomat biletomat){
         this.biletomat = biletomat;
+        this.licznikCzynnosciSerwisowych++;
     }
 
 
     public void setDate(LocalDate data){
         this.biletomat.data = data;
+        this.licznikCzynnosciSerwisowych++;
     }
 
     public void setLokalizacja(String lokalizacja){
         this.biletomat.lokalizacja = lokalizacja;
+        this.licznikCzynnosciSerwisowych++;
     }
     public void setIloscBlankietow(int iloscBlankietow){
         this.biletomat.iloscBlankietow = iloscBlankietow;
+        this.licznikCzynnosciSerwisowych++;
     }
     public void setIloscMonet(int iloscMonet){
         this.biletomat.iloscMonet = iloscMonet;
+        this.licznikCzynnosciSerwisowych++;
     }
 
     public void setBilet(Bilet bilet, int cena, RodzajBiletu rodzaj){
         bilet.cena = cena;
         bilet.rodzaj = rodzaj;
+        this.licznikCzynnosciSerwisowych++;
     }
 }
