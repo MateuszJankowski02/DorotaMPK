1439a7e7864a0f27254ea69039f0aeba49c4bb2e
MateuszJankowski02 <twonightdragons@gmail.com>
2023-11-23 17:02:04 +0100

    Obsrany biletomat

--- src/Biletomat/Biletomat.java
+++ src/Biletomat/Biletomat.java
@@ -1,53 +1,29 @@
 package Biletomat;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 
 public class Biletomat {
 
-    private List<Bilet> sprzedaneBilety;  // Nowa lista do przechowywania sprzedanych biletów
+    int iloscBlankietow, iloscMonet;
+    String lokalizacja;
+    LocalDate data;
 
-    public Biletomat() {
-        this.sprzedaneBilety = new ArrayList<>();
-        Biletomat biletomat = new Biletomat();
-
-// Tworzenie biletów
-        Bilet bilet1 = biletomat.createBilet20min(RodzajBiletu.normalny);
-        Bilet bilet2 = biletomat.createBilet60min(RodzajBiletu.ulgowy);
-
-// Dodawanie do listy sprzedanych biletów
-        biletomat.dodajSprzedanyBilet(bilet1);
-        biletomat.dodajSprzedanyBilet(bilet2);
-
-// Wydruk wszystkich sprzedanych biletów
-        biletomat.wydrukujSprzedaneBilety();
+    public Biletomat(int iloscBlankietow, int iloscMonet, String lokalizacja){
+        this.iloscBlankietow = iloscBlankietow;
+        this.iloscMonet = iloscMonet;
+        this.lokalizacja = lokalizacja;
+        this.data = LocalDate.now();
     }
 
-    public Bilet createBilet20min(RodzajBiletu rodzaj) {
+
+    public Bilet createBilet20min(RodzajBiletu rodzaj){
         return new Bilet20min(rodzaj);
     }
-
-    public Bilet createBilet60min(RodzajBiletu rodzaj) {
+    public Bilet createBilet60min(RodzajBiletu rodzaj ){
         return new Bilet60min(rodzaj);
     }
 
-    // Metoda do dodawania sprzedanego biletu do listy
-    public void dodajSprzedanyBilet(Bilet bilet) {
-        sprzedaneBilety.add(bilet);
+    public Serwis createSerwis(){
+        return new Serwis();
     }
-
-    // Metoda do wydruku wszystkich sprzedanych biletów
-    public void wydrukujSprzedaneBilety() {
-        for (Bilet bilet : sprzedaneBilety) {
-            System.out.println(bilet);
-        }
-    }
-    Biletomat biletomat = new Biletomat();
-
-    // Tworzenie biletów
-    Bilet bilet1 = biletomat.createBilet20min(RodzajBiletu.normalny);
-    Bilet bilet2 = biletomat.createBilet60min(RodzajBiletu.ulgowy);
-
-
-
 }
