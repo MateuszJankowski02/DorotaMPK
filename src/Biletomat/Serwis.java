7bb9b0f87fb4e8821a3580de755555c56214e15c
KamilJag02 <kamil.jagielski@student.pk.edu.pl>
2023-11-23 17:57:25 +0100

    Changes (something) to class Serwis

--- src/Biletomat/Serwis.java
+++ src/Biletomat/Serwis.java
@@ -7,33 +7,42 @@ public class Serwis {
     int licznikCzynnosciSerwisowych;
     Biletomat biletomat;
 
-    protected Serwis(Biletomat biletomat){
+    protected Serwis(Biletomat biletomat) {
         this.biletomat = biletomat;
         this.licznikCzynnosciSerwisowych++;
     }
 
 
-    public void setDate(LocalDate data){
+    public void setDate(LocalDate data) {
         this.biletomat.data = data;
         this.licznikCzynnosciSerwisowych++;
     }
 
-    public void setLokalizacja(String lokalizacja){
+    public void setLokalizacja(String lokalizacja) {
         this.biletomat.lokalizacja = lokalizacja;
         this.licznikCzynnosciSerwisowych++;
     }
-    public void setIloscBlankietow(int iloscBlankietow){
+
+    public void setIloscBlankietow(int iloscBlankietow) {
         this.biletomat.iloscBlankietow = iloscBlankietow;
         this.licznikCzynnosciSerwisowych++;
     }
-    public void setIloscMonet(int iloscMonet){
+
+    public void setIloscMonet(int iloscMonet) {
         this.biletomat.iloscMonet = iloscMonet;
         this.licznikCzynnosciSerwisowych++;
     }
 
-    public void setBilet(Bilet bilet, int cena, RodzajBiletu rodzaj){
+    public void setBilet(Bilet bilet, int cena, RodzajBiletu rodzaj) {
         bilet.cena = cena;
         bilet.rodzaj = rodzaj;
         this.licznikCzynnosciSerwisowych++;
     }
+    @Override
+    public String toString() {
+
+        return "Licznik czynnosci serwisowych: " +licznikCzynnosciSerwisowych+ "\n" + "Lokazlizacja: " + this.biletomat.lokalizacja +"\n"+
+       "Data: "+ this.biletomat.data+"\n"+"Ilosc blaniketow: "+ this.biletomat.iloscBlankietow+"\n"+"Ilosc monet: "+ this.biletomat.iloscMonet;
+    }
 }
+
